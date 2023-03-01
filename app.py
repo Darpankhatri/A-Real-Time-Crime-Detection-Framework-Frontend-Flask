558419744b8fd228ac19cb5eb7bb4ea3db024217
Darpan Kumar <darpankhatri817@gmail.com>
2023-03-01 18:09:19 +0500

    camera update

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask, request, render_template, redirect, url_for
-# import cv2
+from flask import Flask, request, render_template, redirect, url_for, Response, jsonify
+import cv2
 app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)
+def gen_frames():  
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 @app.route("/")
 def hello():
@@ -29,9 +41,20 @@ def predict():
 def predictlive():
     return render_template('livepredict.html')
 
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route("/our-work")
 def ourwork():
     return render_template('ourwork.html')
+
+@app.route('/done')
+def done():
+    if camera.isOpened():
+        print("Releasing cam feed")
+        camera.release()
+    return jsonify("done")
+
 
 app.run(debug = True)

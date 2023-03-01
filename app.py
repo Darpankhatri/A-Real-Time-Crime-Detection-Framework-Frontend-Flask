7861ecb274881dc14fb42820fd49332a3612fa48
Darpan Kumar <darpankhatri817@gmail.com>
2023-03-01 16:37:38 +0500

    update

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, render_template, redirect, url_for
+# import cv2
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    return redirect(url_for('index'))
 
 @app.route("/<name>")
 def check(name):
@@ -13,13 +14,24 @@ def check(name):
 @app.route("/about")
 def about():
     var1 = "Darpan"
-    
-    # return "Index1"
     return render_template('about.html',varval = var1)
 
 
 @app.route("/index")
 def index():
     return render_template('index.html')
+
+@app.route("/predict")
+def predict():
+    return render_template('predict.html')
+
+@app.route("/live-predict")
+def predictlive():
+    return render_template('livepredict.html')
+
+
+@app.route("/our-work")
+def ourwork():
+    return render_template('ourwork.html')
 
 app.run(debug = True)
